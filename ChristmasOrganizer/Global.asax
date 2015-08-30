144190c30a7cebe1e6d459d1b4a932b07a14124a
Mpogson <mpogson@paraport.com>
2015-08-30 15:57:40 -0700

    initial project set up

---
+++ ChristmasOrganizer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChristmasOrganizer.MvcApplication" Language="C#" %>
---
+++ ChristmasOrganizer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChristmasOrganizer.Startup))]
+namespace ChristmasOrganizer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
